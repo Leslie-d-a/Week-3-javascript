d47ce1b063a0d01f40e75fadff4cc0b0003cd86e
Leslie-d-a <990626288@mydavinci.nl>
2020-09-09 13:00:47 +0200

    do while verandert naar een for loop

--- lab 3/js/lab3 1,2.js
+++ lab 3/js/lab3 1,2.js
@@ -7,10 +7,8 @@ result = 0;
 multiply = 0;
 multiply = prompt("nummer");
 
-do {
-	result = +result + +multiply;
+for (i = 0; i < 10; i++) {
+	result = Number(result) + Number(multiply);
 	number = i + 1;
-	document.write(number.toString() + " * " + multiply + " = " + result + "<br>");
-	i++;
-}
-while (i < 10);
+	document.write(number.toString() + " * " + multiply + " = " + result + "<br>");;
+};
